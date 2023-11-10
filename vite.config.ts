fbfc994127e6bda6fab94bcd87b25e3bc22ff8dc
thaboRach <masipathabo.rachidi@vcontractor.co.za>
2023-11-10 13:44:23 +0200

    Update config and create gh depoloyment

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { nodePolyfills } from 'vite-plugin-node-polyfills';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), nodePolyfills()],
-	server: {
-		open: true,
-	},
+  plugins: [react(), nodePolyfills()],
+  server: {
+    open: true,
+  },
+  base: "/image-scanner/",
 });
